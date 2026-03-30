d59551a7c4ddfb7b038b46eef7c0cf51e5c60e3c
Anastasiia Kovalchuk <anastasiia.kovalchuk@factum-ua.com>
2026-03-31 01:03:10 +0300

    solver#1

--- main.py
+++ main.py
@@ -21,12 +21,45 @@ def calculate_w(F, G, x, y, module):
     W = (F*x + G*y) % module
     return W
 
-F = bin_list_to_number(generate_number(256, 10))
-G = bin_list_to_number(generate_number(256, 10))
-x = bin_list_to_number(generate_number(256, 10))
-y = bin_list_to_number(generate_number(256, 10))
+# F = bin_list_to_number(generate_number(256, 10))
+# G = bin_list_to_number(generate_number(256, 10))
+# x = bin_list_to_number(generate_number(256, 10))
+# y = bin_list_to_number(generate_number(256, 10))
 
-print(F)
-print(len(bin(F)[2:]))
-print(bin(F)[2:].count('1'))
-print(bin(F)[2:].count('0'))
+# print(F)
+# print(len(bin(F)[2:]))
+# print(bin(F)[2:].count('1'))
+# print(bin(F)[2:].count('0'))
+
+class Solver1():
+
+    def __init__(self, n, m, h):
+        self.n = n
+        self.m = m
+        self.h = h
+        self.GM = get_module(self.n, self.m)
+        self.F = bin_list_to_number(generate_number(self.n, self.h))
+        self.G = bin_list_to_number(generate_number(self.n, self.h))
+        self.x = bin_list_to_number(generate_number(self.n, self.h))
+        self.y = bin_list_to_number(generate_number(self.n, self.h))
+        self.W = calculate_w(self.F, self.G, self.x, self.y, self.GM)
+    
+    def recover_x(self):
+        res = []
+        for r in range(self.n):
+            delta_r = (self.W).bit_count() - ((self.W - ((2**r * self.F) % self.GM)) % self.GM).bit_count()
+            if (r >= (self.n - self.m)) and (0 < delta_r <= (self.h + r - self.n + self.m)):
+                res.append('1')
+            elif (r < (self.n - self.m)) and (0 < delta_r <= self.h):
+                res.append('1')
+            else:
+                res.append('0')
+        return res
+
+solve1 = Solver1(128, 72, 10)
+x = bin(solve1.x)[2:]
+x_true = [i for i in x]
+print(x_true)
+x_recovered = solve1.recover_x()
+print(x_recovered)
+print(x_recovered.count('1'))
